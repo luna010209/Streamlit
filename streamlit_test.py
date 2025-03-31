ac4eed858ca7808cdfc5875cc5e6c87edafeefcc
Lunado <liendhhha140217@gmail.com>
2025-03-31 23:20:40 +0900

    Basic streamlit

---
+++ streamlit_test.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# st.write("Hello Streamlit!!!")
+# st.write({"key":["value1", "value2"]})
+
+st.title("Simple Data Dasboard")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+  st.write("File uploaded...")
+  df = pd.read_csv(uploaded_file)
+
+  st.subheader("Data Preview")
+  st.write(df.head())
+
+  st.subheader("Data Summary")
+  st.write(df.describe())
+
+  st.subheader("Filter Data")
+  columns = df.columns.tolist()
+  selected_column = st.selectbox("Select column to filter by", columns)
+  unique_values = df[selected_column].unique()
+  selected_value = st.selectbox("Select value", unique_values)
+
+  filtered_df = df[df[selected_column]==selected_value]
+  st.write(filtered_df)
+
+  st.subheader("Plot data")
+  x_column = st.selectbox("Select a-axis column", columns)
+  y_column = st.selectbox("Select y-axis column", columns)
+
+  if st .button("Generate Plot"):
+    st.line_chart(filtered_df.set_index(x_column)[y_column])
+  elif st.button("Bar chart"):
+    st.bar_chart(filtered_df.set_index(x_column)[y_column])
+
+else:
+  st.write("Waiting for uploading file")
